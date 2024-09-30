4e2d19ec2f89f56ac6e8e3a68d60cea1f50cf633
Ciro Guilherme Nass <ciroguilhermenass@gmail.com>
2024-09-29 22:05:58 -0300

    aula1java

--- POOTreino/PrimeiroCodigo/PrimeiroCodigo.java
+++ POOTreino/PrimeiroCodigo/PrimeiroCodigo.java
@@ -1,5 +1,9 @@
 package SegundoAno.POOTreino.PrimeiroCodigo;
 
 public class PrimeiroCodigo {
-    
+    public static void main(String[] args){
+        System.out.println("Meu primeiro código em Java!");
+        
+        System.out.println("Aula de POO");
+    }
 }
