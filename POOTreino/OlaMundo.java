a18d946b196b92e90f4db6c8e53bc56df88e3f64
Ciro Guilherme Nass <ciroguilhermenass@gmail.com>
2024-09-29 21:19:06 -0300

    testepoo

---
+++ POOTreino/OlaMundo.java
@@ -0,0 +1,7 @@
+package SegundoAno.POOTreino;
+
+public class OlaMundo {
+    public static void main(String[] args){
+        System.out.println("Olá Mundo");
+    }
+}
--- VisaoComp/aula2/aula2.py
+++ VisaoComp/aula2/aula2.py
@@ -1,17 +1,17 @@
-# import cv2
-# import numpy as np
+import cv2
+import numpy as np
 # imagem original
-img = cv2.imread('praia.jpg')
-cv2.imshow('ORIGINAL', img)
+img = cv2.imread('C:/Users/cirog/source/2Ano/VisaoComp/Aula2/praia.jpg')
+cv2.imshow('praia', img)
 cv2.waitKey(0)
 #conversao para formato RGB
 rgbimg = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-# cv2.imshow('RGB', rgbimg)
-# cv2.waitKey(0)
+cv2.imshow('RGB', rgbimg)
+cv2.waitKey(0)
 # conversao para formato em nivel de cinza
 imggray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# cv2.imshow('Nivel de cinza', imggray)
-# cv2.waitKey(0)
+cv2.imshow('Nivel de cinza', imggray)
+cv2.waitKey(0)
 # binarizacao imagem em nivel de cinza
 (thres, imgba) = cv2.threshold(imggray, 155, 255, cv2.THRESH_OTSU)
 cv2.imshow('Binarizada', imgba)
