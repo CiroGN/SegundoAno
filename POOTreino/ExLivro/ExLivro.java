e72ce6fcfea71dd4ac056954a8b6ec59b3905e14
Ciro Guilherme Nass <ciroguilhermenass@gmail.com>
2024-10-02 07:26:13 -0300

    exlivro

---
+++ POOTreino/ExLivro/ExLivro.java
@@ -0,0 +1,14 @@
+package POOTreino.ExLivro;
+
+public class ExLivro {
+    public static void main(String[] args) {
+        public String livro, autor;
+        public int anoPublicacao;
+
+        ExLivro(String livro, String autor, int anoPublicacao){
+            this.livro = livro;
+            this.autor = autor;
+            this.anoPublicacao = anoPublicacao;
+        }
+    }
+}
