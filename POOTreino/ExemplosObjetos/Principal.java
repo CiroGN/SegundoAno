4a8dd0746c2e88c03f950921110186e7bee877d7
Ciro Guilherme Nass <ciroguilhermenass@gmail.com>
2024-10-02 07:05:04 -0300

    atu

---
+++ POOTreino/ExemplosObjetos/Principal.java
@@ -0,0 +1,9 @@
+package SegundoAno.POOTreino.ExemplosObjetos;
+
+public class Principal {
+    public static void main(String[] args){
+        Pessoa pessoa = new Pessoa("Ciro", 19);
+        
+        pessoa.exibirInformacoes();
+    }
+}
