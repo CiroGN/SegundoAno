6d36c8df6374bae409e587fa6f96bddf3ca6b8fc
Ciro Guilherme Nass <ciroguilhermenass@gmail.com>
2024-12-13 21:53:02 -0300

    termino dataset500

--- VisaoComp/IdentiFace/500/treino.py
+++ VisaoComp/IdentiFace/500/treino.py
@@ -1,26 +1,27 @@
 from ultralytics import YOLO
 
 if __name__ == "__main__":
-    # Carrega o modelo YOLO
-    model = YOLO("yolo11n.pt")
+    # # Carrega o modelo YOLO
+    # model = YOLO("yolo11n.pt")
 
-    # Treina o modelo
-    results = model.train(
-        data="C:/Users/cirog/source/SegundoAno/VisaoComp/IdentiFace/500/dataset500/data.yaml",
-        epochs=30,
-        imgsz=640,
-        project="SegundoAno/VisaoComp/IdentifFace/dataset500",
-        name = ""
-    )
+    # # Treina o modelo
+    # results = model.train(
+    #     data="C:/Users/cirog/source/SegundoAno/VisaoComp/IdentiFace/500/dataset500/data.yaml",
+    #     epochs=30,
+    #     imgsz=640,
+    #     project="SegundoAno/VisaoComp/IdentiFace/500",
+    #     name = "modelo"
+    # )
 
     # Após o treinamento, carregue o modelo salvo para realizar previsões
-    trained_model = YOLO("SegundoAno/VisaoComp/IdentifFace/dataset500/train/weights/best.pt")
+    trained_model = YOLO("SegundoAno/VisaoComp/IdentiFace/500/modelo/weights/best.pt")
 
     # Realiza a previsão usando o modelo treinado
     predictions = trained_model.predict(
-        source="SegundoAno/VisaoComp/IdentifFace/dataset500/032_CIEE_FORMATURA_30092022.JPG",
+        source="SegundoAno/VisaoComp/IdentiFace/500/image.png",
         save=True,
         imgsz=320,
         conf=0.5,
-        project="SegundoAno/VisaoComp/IdentifFace/dataset500"
+        project="SegundoAno/VisaoComp/IdentiFace/500/dataset500",
+        name = "previsao"
     )
