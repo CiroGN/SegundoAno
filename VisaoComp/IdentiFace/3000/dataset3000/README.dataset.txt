47fd4111eb8ce1eb9a7c3db47ad7cf164032d3a4
Ciro Guilherme Nass <ciroguilhermenass@gmail.com>
2024-12-13 22:06:32 -0300

    dataset3000

---
+++ VisaoComp/IdentiFace/3000/dataset3000/README.dataset.txt
@@ -0,0 +1,6 @@
+# IdentiFace-3k > 2024-12-13 1:40pm
+https://universe.roboflow.com/cirogn/identiface-3k
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ VisaoComp/IdentiFace/3000/treino.py
@@ -0,0 +1,27 @@
+from ultralytics import YOLO
+
+if __name__ == "__main__":
+    # Carrega o modelo YOLO
+    model = YOLO("yolo11n.pt")
+
+    # Treina o modelo
+    results = model.train(
+        data="C:/Users/cirog/source/SegundoAno/VisaoComp/IdentiFace/500/dataset500/data.yaml",
+        epochs=30,
+        imgsz=640,
+        project="SegundoAno/VisaoComp/IdentiFace/500",
+        name = "modelo"
+    )
+
+    # Após o treinamento, carregue o modelo salvo para realizar previsões
+    trained_model = YOLO("SegundoAno/VisaoComp/IdentiFace/500/modelo/weights/best.pt")
+
+    # Realiza a previsão usando o modelo treinado
+    predictions = trained_model.predict(
+        source="SegundoAno/VisaoComp/IdentiFace/500/image.png",
+        save=True,
+        imgsz=320,
+        conf=0.5,
+        project="SegundoAno/VisaoComp/IdentiFace/500/dataset500",
+        name = "previsao"
+    )
