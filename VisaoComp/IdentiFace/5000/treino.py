628260ca4b0b12b201fdbf1dd555c4de0cd8cf53
Ciro Guilherme Nass <ciroguilhermenass@gmail.com>
2024-12-14 16:26:06 -0300

    termino datasets

--- VisaoComp/IdentiFace/5000/treino.py
+++ VisaoComp/IdentiFace/5000/treino.py
@@ -2,23 +2,23 @@ from ultralytics import YOLO
 
 if __name__ == "__main__":
     # Carrega o modelo YOLO
-    model = YOLO("yolo11n.pt")
+    # model = YOLO("yolo11n.pt")
 
-    # Treina o modelo
-    results = model.train(
-        data="C:/Users/cirog/source/SegundoAno/VisaoComp/IdentiFace/5000/dataset5000/data.yaml",
-        epochs=30,
-        imgsz=640,
-        project="SegundoAno/VisaoComp/IdentiFace/5000",
-        name = "modelo"
-    )
+    # # Treina o modelo
+    # results = model.train(
+    #     data="C:/Users/cirog/source/SegundoAno/VisaoComp/IdentiFace/5000/dataset5000/data.yaml",
+    #     epochs=30,
+    #     imgsz=640,
+    #     project="SegundoAno/VisaoComp/IdentiFace/5000",
+    #     name = "modelo"
+    # )
 
     # Após o treinamento, carregue o modelo salvo para realizar previsões
     trained_model = YOLO("SegundoAno/VisaoComp/IdentiFace/5000/modelo/weights/best.pt")
 
     # Realiza a previsão usando o modelo treinado
     predictions = trained_model.predict(
-        source="SegundoAno/VisaoComp/IdentiFace/5000/image.png",
+        source="SegundoAno/VisaoComp/IdentiFace/5000/imagem.jpg",
         save=True,
         imgsz=320,
         conf=0.5,
